94f3f256a4a979e24f5977461c7434d577705e30
ItsAMeAMario <jonahdaves11@gmail.com>
2025-10-20 20:46:18 -0400

    made crud

--- CRUD/index.js
+++ CRUD/index.js
@@ -1,4 +1,53 @@
 
-function myFunction() {
-    document.getElementById("rizz").innerHTML = document.getElementById("Jerry").value + 5
+function save(name, value) {
+
+    localStorage.setItem(name, value)
 }
+
+
+function load(name) {
+    var item = localStorage.getItem(name) || ""
+    return item
+}
+
+
+function message(value) {
+
+    document.getElementById("message").innerHTML = value
+}
+
+
+function create() {
+    var item = load(document.getElementById("name").value)
+    if (item) {
+        message("item with that name already exists")
+    } else { }
+    save(document.getElementById("name").value, document.getElementById("color").value)
+}
+
+function update() {
+    var item = load(document.getElementById("name").value)
+    if (!item) {
+        message("item not available")
+    } else {
+        create()
+    }
+}
+
+
+function read() {
+
+    message(load(document.getElementById("name").value) || "not found")
+}
+
+function erase() {
+
+    localStorage.removeItem(document.getElementById("name").value)
+}
+
+function eraseall() {
+
+    localStorage.clear()
+}
+
+
